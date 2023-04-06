07c181dbbebd322e33007205ce2afa613b2dfa98
Ajay M V <104972680+Ajay7306@users.noreply.github.com>
2023-04-07 00:01:54 +0530

    font changed

--- ETH-tokyo Frontend/components/Layout.jsx
+++ ETH-tokyo Frontend/components/Layout.jsx
@@ -77,7 +77,7 @@ const Layout = ({ children }) => {
                             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                             <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"  rel="stylesheet" />
                         </Head>
-                        <div className="flex h-full flex-row  items-start w-full  bg-secondary-white">
+                        <div className="font-Poppins flex h-full flex-row  items-start w-full  bg-secondary-white">
                         <SideBar />
                             {children}
                             {/* <ConnectModal /> */}
